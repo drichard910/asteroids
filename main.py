786a7cea834a38aa364089125bf83cce33e96bb3
drichard910 <dorian910.dr@gmail.com>
2025-03-11 16:49:20 -0300

    Stated asteroids project, created main.py constant

---
+++ main.py
@@ -0,0 +1,19 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+def main():
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen_fill_counter = 0
+    while screen_fill_counter >= 0:
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                        return
+        pygame.Surface.fill(screen, (0, 0, 0))
+        screen_fill_counter += 1
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
