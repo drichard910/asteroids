919413845565e2ecb3d6c6815728429daf0d0ccb
drichard910 <dorian910.dr@gmail.com>
2025-09-16 11:27:47 -0300

    added framerate

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen_fill_counter = 0
+    clock = pygame.time.Clock
+    dt = 0
+    
     while screen_fill_counter >= 0:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -15,6 +18,8 @@ def main():
         pygame.Surface.fill(screen, (0, 0, 0))
         screen_fill_counter += 1
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
